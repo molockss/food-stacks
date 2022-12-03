4931e21cdce1dbedd54a14c2dbfdd6fd72a64406
Mahamed Osman <mahamed_67@hotmail.com>
2022-12-03 09:30:57 +0400

    issue with client id fixed

--- config/google.js
+++ config/google.js
@@ -1,48 +1,48 @@
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-const mongoose = require('mongoose')
-const Googleuser = require('../models/Google')
-const passport = require('passport');
+// const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+// const mongoose = require('mongoose')
+// const Googleuser = require('../models/Google')
+// const passport = require('passport');
 
 
-module.exports = function (passport) {
-    passport.use(
-      new GoogleStrategy(
-        {
-          clientID: process.env.GOOGLE_CLIENT_ID
-          clientSecret: process.env.GOOGLE_CLIENT_SECRET
-          callbackURL: '/auth/google/callback',
-        },
-        async (accessToken, refreshToken, profile, done) => {
-          const newUser = {
-            googleId: profile.id,
-            displayName: profile.displayName,
-            firstName: profile.name.givenName,
-            lastName: profile.name.familyName,
-            image: profile.photos[0].value,
-          }
+// module.exports = function (passport) {
+//     passport.use(
+//       new GoogleStrategy(
+//         {
+//           clientID: process.env.GOOGLE_CLIENT_ID
+//           clientSecret: process.env.GOOGLE_CLIENT_SECRET
+//           callbackURL: '/auth/google/callback',
+//         },
+//         async (accessToken, refreshToken, profile, done) => {
+//           const newUser = {
+//             googleId: profile.id,
+//             displayName: profile.displayName,
+//             firstName: profile.name.givenName,
+//             lastName: profile.name.familyName,
+//             image: profile.photos[0].value,
+//           }
   
-          try {
-            let user = await User.findOne({ googleId: profile.id })
+//           try {
+//             let user = await User.findOne({ googleId: profile.id })
   
-            if (user) {
-              done(null, user)
-            } else {
-              user = await User.create(newUser)
-              done(null, user)
-            }
-          } catch (err) {
-            console.error(err)
-          }
-        }
-      )
-    )
+//             if (user) {
+//               done(null, user)
+//             } else {
+//               user = await User.create(newUser)
+//               done(null, user)
+//             }
+//           } catch (err) {
+//             console.error(err)
+//           }
+//         }
+//       )
+//     )
   
-    passport.serializeUser((user, done) => {
-      done(null, user.id)
-    })
+//     passport.serializeUser((user, done) => {
+//       done(null, user.id)
+//     })
   
-    passport.deserializeUser((id, done) => {
-      User.findById(id, (err, user) => done(err, user))
-    })
-  }
+//     passport.deserializeUser((id, done) => {
+//       User.findById(id, (err, user) => done(err, user))
+//     })
+//   }
   
--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const logger = require("morgan");
 // const connectDB = require("./config/database");
 const mainRoutes = require("./routes/main");
 const postRoutes = require("./routes/posts");
-// const google = require("./config/google");
+const googleRoutes = require('./routes/google');
+
 
 //Use .env file in config folder
 require("dotenv").config();
@@ -56,15 +57,15 @@ app.use(passport.session());
 /**
  * OAuth authentication routes. (Sign in)
  */
- app.get('/auth/google', 
- passport.authenticate('google', { scope : ['profile', 'email'] }));
+//  app.get('/auth/google', 
+//  passport.authenticate('google', { scope : ['profile', 'email'] }));
 
-app.get('/auth/google/callback', 
- passport.authenticate('google', { failureRedirect: '/error' }),
- function(req, res) {
-   // Successful authentication, redirect success.
-   res.redirect('/index.ejs');
- });
+// app.get('/auth/google/callback', 
+//  passport.authenticate('google', { failureRedirect: '/error' }),
+//  function(req, res) {
+//    // Successful authentication, redirect success.
+//    res.redirect('/index.ejs');
+//  });
 
 //  app.get('/success', (req, res) => res.send(feed.ejs));
 
@@ -74,6 +75,7 @@ app.use(flash());
 //Setup Routes For Which The Server Is Listening
 app.use("/", mainRoutes);
 app.use("/post", postRoutes);
+app.use('/auth', googleRoutes)
 
 //Server Running
 app.listen(process.env.PORT || PORT, () => {
@@ -85,15 +87,15 @@ app.listen(process.env.PORT || PORT, () => {
 
 
 
-app.use(session({
-  resave: false,
-  saveUninitialized: true,
-  secret: 'SECRET' 
-}));
+// app.use(session({
+//   resave: false,
+//   saveUninitialized: true,
+//   secret: 'SECRET' 
+// }));
 
-app.get('/', function(req, res) {
-  res.render('pages/auth');
-});
+// app.get('/', function(req, res) {
+//   res.render('pages/auth');
+// });
 
 
 
@@ -103,41 +105,41 @@ app.get('/', function(req, res) {
 
 
 
-app.get('/success', (req, res) => res.send(userProfile));
-app.get('/error', (req, res) => res.send("error logging in"));
+// app.get('/success', (req, res) => res.send(userProfile));
+// app.get('/error', (req, res) => res.send("error logging in"));
 
-passport.serializeUser(function(user, cb) {
-  cb(null, user);
-});
+// passport.serializeUser(function(user, cb) {
+//   cb(null, user);
+// });
 
-passport.deserializeUser(function(obj, cb) {
-  cb(null, obj);
-});
+// passport.deserializeUser(function(obj, cb) {
+//   cb(null, obj);
+// });
 
 /*  Google AUTH  */
  
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-const GOOGLE_CLIENT_ID = "49011582156-l0aar1csjtupd65s6rr35k26441fabqt.apps.googleusercontent.com" 
-const GOOGLE_CLIENT_SECRET = "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"
-passport.use(new GoogleStrategy({
-    clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-      userProfile=profile;
-      return done(null, userProfile);
-  }
-));
+// const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+// const GOOGLE_CLIENT_ID = "49011582156-l0aar1csjtupd65s6rr35k26441fabqt.apps.googleusercontent.com" 
+// const GOOGLE_CLIENT_SECRET = "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"
+// passport.use(new GoogleStrategy({
+//     clientID: GOOGLE_CLIENT_ID,
+//     clientSecret: GOOGLE_CLIENT_SECRET,
+//     callbackURL: "http://localhost:3000/auth/google/callback"
+//   },
+//   function(accessToken, refreshToken, profile, done) {
+//       userProfile=profile;
+//       return done(null, userProfile);
+//   }
+// ));
  
-app.get('/auth/google', 
-  passport.authenticate('google', { scope : ['profile', 'email'] }));
+// app.get('/auth/google', 
+//   passport.authenticate('google', { scope : ['profile', 'email'] }));
  
-app.get('/auth/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/error' }),
-  function(req, res) {
-    // Successful authentication, redirect success.
-    res.redirect('/success');
-  });
+// app.get('/auth/google/callback', 
+//   passport.authenticate('google', { failureRedirect: '/error' }),
+//   function(req, res) {
+//     // Successful authentication, redirect success.
+//     res.redirect('/success');
+//   });
 
-  app.get('/success', (req, res) => res.send(feed.ejs));
+//   app.get('/success', (req, res) => res.send(feed.ejs));
